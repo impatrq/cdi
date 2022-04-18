1038461f8d2d432967a0d2f7b88592780a5bafe4
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-04-18 09:17:15 -0300

    Create readability.c

---
+++ readability.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+#define MAX 4096
+
+int read_file(char *text, char *filename);
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Uso: ./readability file\n");
+        return 1;
+    }
+
+    char text[MAX];
+
+    if (read_file(text, argv[1])) {
+        printf("Archivo no encontrado\n");
+        return 1;
+    }
+
+    // TODO
+
+    return 0;
+}
+
+int read_file(char *text, char *filename) {
+
+    FILE *fp;
+    char c;
+    char full_name[30];
+    
+    sprintf(full_name, "texts/%s.txt", filename);
+    fp = fopen(full_name, "r");
+    
+    if (!fp) {
+        return 1;
+    } 
+    while ((c = getc(fp)) != EOF) {
+        *text = c;
+        text++;
+    }
+    return 0;
+}
